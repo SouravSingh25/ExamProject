46082ec71872c82243ec11723b40cfad15fa61fa
SouravSingh25 <sourav25.parmar@gmail.com>
2020-02-18 13:48:31 +0530

    Updated Push

--- exam/src/main/java/com/capgemini/exam/client/CgUtil.java
+++
@@ -1,8 +0,0 @@
-package com.capgemini.exam.client;
-
-import java.util.Scanner;
-
-public class CgUtil {
-	public static Scanner scan = new Scanner(System.in);
-
-}
--- exam/src/main/java/com/capgemini/exam/client/ExamClient.java
+++ exam/src/main/java/com/capgemini/exam/client/ExamClient.java
@@ -1,31 +1,10 @@
 package com.capgemini.exam.client;
 
-import com.capgemini.exam.dao.ExamDao;
-import com.capgemini.exam.dao.ExamDaoImpl;
-import com.capgemini.exam.service.ExamService;
-import com.capgemini.exam.service.ExamServiceImpl;
-
 public class ExamClient {
-	
+
 	public static void main(String[] args) {
-		ExamDao obj1=new ExamDaoImpl();
-		ExamService obj2=new ExamServiceImpl();
 		
-		System.out.println("Enter the Exam Id:");
-		Integer id=CgUtil.scan.nextInt();
-		System.out.println("Enter the question id:");
-		Integer qid=CgUtil.scan.nextInt();	
-		System.out.println("Enter the question:");
-		String ques=CgUtil.scan.next();
-		System.out.println("Enter the options:");
-		String []opt = null  ;
-		for(int i=0;i<4;i++)
-			opt[i]=CgUtil.scan.next();
-		System.out.println("Enter the Answer:");
-		String ans=CgUtil.scan.nextLine();
-		System.out.println("Enter the Max. Marks:");
-		Integer marks=CgUtil.scan.nextInt();
-		
-	
+
 	}
+
 }
